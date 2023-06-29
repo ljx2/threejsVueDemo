3134a41f3650d9f11382b43262ee8d06b26be6dd
longjinxiang <longjinxiang@konka.com>
2023-06-29 11:41:32 +0800

    feat: 模型使用轨道控制器

--- src/App2.vue
+++ src/App2.vue
@@ -1,42 +1,36 @@
 <script setup>
 import * as THREE from 'three'; // 全局引入
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 500);
-camera.lookAt(0, 0, 0);
-
 const scene = new THREE.Scene();
 
-// const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-// const points = [];
-// points.push(new THREE.Vector3(- 10, 0, 0));
-// points.push(new THREE.Vector3(0, 10, 0));
-// points.push(new THREE.Vector3(10, 0, 0));
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.set(0, 0, 500);
+camera.lookAt(100, 0, 0);
 
-// const geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-// const line = new THREE.Line(geometry, material);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
 const loader = new GLTFLoader();
-
 loader.load('src/model/scene.gltf', function (gltf) {
   scene.add(gltf.scene);
-  // scene.add(line);
-  renderer.render(scene, camera);
+  function animate() {
+    requestAnimationFrame(animate);
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    controls.update();
+    renderer.render(scene, camera);
+  }
+  animate()
 }, function (ProgressEvent) {
   console.log(ProgressEvent);
 }, function (error) {
-
   console.error(error);
-
 });
-
-
 
 </script>
 
@@ -44,5 +38,4 @@ loader.load('src/model/scene.gltf', function (gltf) {
 </template>
 
 <style scoped>
-
 </style>
